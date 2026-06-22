3304879493154d1eb76d8845dc0148ac51f5f340
KateVol99 <69426894+KateVol99@users.noreply.github.com>
2026-06-22 16:32:01 +0300

    Create 04_data_preparation.sql
    Performs initial data quality assessment by checking dataset size, identifying missing values, and detecting potential duplicate records before fraud analysis.

---
+++ sql/04_data_preparation.sql
@@ -0,0 +1,15 @@
+--Table size
+SELECT COUNT (*) AS total_rows
+FROM transactions;
+
+--Missing Values
+SELECT
+    COUNT (*) FILTER (WHERE "Amount" IS NULL) AS amount_missing,
+	COUNT (*) FILTER (WHERE "Class" IS NULL) AS class_missing
+FROM transactions;
+
+--Duplicates
+SELECT COUNT (*) - 
+       COUNT(DISTINCT("Time", "Amount", "Class"))
+	   AS duplicate_count
+FROM transactions;
